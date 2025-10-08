ef50eeeb676dc55df3fd6d19818ddc94193f0243
Blaise202 <iblaise2023@gmail.com>
2025-10-08 23:35:38 +0000

    Predevined variables

--- BankAccount.java
+++ BankAccount.java
@@ -5,6 +5,25 @@ public class BankAccount{
     SAVINGS
   }
 
+  public AccountType acctType;
+  public String acctId;
+  public double balance;
+  public int numWithdrawals;
+  public boolean inTheRed;
+  public double minBalance;
+
+  public double CURRENT_ACCT_MIN_BALANCE;
+  public double SAVINGS_ACCT_MIN_BALANCE;
+
+  public double interestRate;
+  public double maintenanceFee;
+  public int withdrawalLimit = -1;
+
+  public double SAVINGS_ACCT_INTEREST_RATE;
+  public double CURRENT_ACCT_MAINTENANCE_FEE;
+  public double SAVINGS_WITHDRAWAL_LIMIT;
+
+
   public static void main(String[] args){
 
   }
